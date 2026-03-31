bf181476ff348463d4cbc8eb3783885122a25a05
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2026-04-01 03:03:28 +0500

    update

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import GeminiHeader from "@/components/Gemini/GeminiHeader";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <main>
+      <GeminiHeader
+        subtitle="AI SKILLS"
+        title="Learn essential AI skills"
+        description="We're building AI skills programs, trainings, and tools to address the specific needs of workers everywhere. Discover Google's courses and resources designed to help you succeed in an AI-driven world."
+      />
+    </main>
+  );
+};
 
-export default page
+export default page;
--- src/components/Gemini/GeminiHeader.tsx
+++ src/components/Gemini/GeminiHeader.tsx
@@ -11,33 +11,47 @@
 // export default GeminiHeader;
 
 type HeaderProps = {
-  title: string;
   subtitle?: string;
+  title: string;
+  description?: string;
   className?: string;
-  titleClassName?: string;
   subtitleClassName?: string;
+  titleClassName?: string;
+  descriptionClassName?: string;
 };
 
 const GeminiHeader = ({
-  title,
   subtitle,
+  title,
+  description,
   className = "",
   titleClassName = "",
   subtitleClassName = "",
+  descriptionClassName = "",
 }: HeaderProps) => {
   return (
-    <div className={`flex flex-col justify-center items-center text-center ${className}`}>
+    <div
+      className={`flex flex-col justify-center items-center gap-4 text-center ${className}`}
+    >
+      {subtitle && (
+        <p
+          className={`text-base sm:text-lg md:text-xl text-gray-700 ${subtitleClassName}`}
+        >
+          {subtitle}
+        </p>
+      )}
+
       <h1
-        className={`flex items-center gap-3 font-normal text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl ${titleClassName}`}
+        className={`flex items-center font-normal text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl ${titleClassName}`}
       >
         {title}
       </h1>
 
-      {subtitle && (
+      {description && (
         <p
-          className={`mt-4 text-sm sm:text-base md:text-lg text-gray-500 ${subtitleClassName}`}
+          className={`max-w-lg text-sm sm:text-base md:text-lg text-gray-500 ${descriptionClassName}`}
         >
-          {subtitle}
+          {description}
         </p>
       )}
     </div>
